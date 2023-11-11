a59a1ddd5d530da8ff505083004eb0cd567b2b58
Keyhan Twellmeyer-Spiegelhauer <keyhan.spiegelhauer2007@gmail.com>
2023-11-11 15:30:08 +0100

    ADD moving platforms
    Bug: Player/Objects on the platform move slower than the platform.
    Currently intended fix: Make the object somehow stick to the platform, so it moves with the paltform.

---
+++ Assets/Scripts/Map/MovingPlatform/MovingPlatform.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [Header("Moving Platform Attributes")]
+    [SerializeField] private float platformSpeed;
+    [SerializeField] private float platformWaitTimeInSec;
+    [SerializeField] private int startingDirection;
+
+    private int horizontalMovingDirection;
+
+    [Header("Start and end position")]
+    [SerializeField] private Transform startPosition;
+    [SerializeField] private Transform endPosition;
+
+    //Components
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (startingDirection == 0)
+            Debug.LogError("This platform has no set starting direction");
+        else
+            horizontalMovingDirection = startingDirection;
+    }
+
+    void Update()
+    {
+        //Check if the start position is reached
+        if(transform.position.x <= startPosition.position.x)
+        {
+            //Stop moving the platform
+            horizontalMovingDirection = 0;
+            StartCoroutine(ChangeMovingDirection(1));
+        } else if (transform.position.x >= endPosition.position.x)
+        {
+            horizontalMovingDirection = 0;
+            StartCoroutine(ChangeMovingDirection(-1));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMovingDirection * platformSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    private IEnumerator ChangeMovingDirection(int newMovingDirection)
+    {
+        yield return new WaitForSeconds(platformWaitTimeInSec);
+
+        horizontalMovingDirection = newMovingDirection;
+    }
+}
